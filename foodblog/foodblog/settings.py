830e75a220ce98d852477b0c2fbb64464aee587d
Jainam <vishyanand10@gmail.com>
2021-04-30 23:27:54 +0530

    third add

--- foodblog/foodblog/settings.py
+++ foodblog/foodblog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -26,7 +26,8 @@ SECRET_KEY = 'el#ir9+gh0l@$)-nayjwp9o&t9rs!&r#80k+brexzy*dzkcj1l'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "foodblogtest.herokuapp.com"
+    "foodblogtest.herokuapp.com",
+    "localhost"
 ]
 
 
@@ -121,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
